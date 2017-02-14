dde52e461d73ac0c318c16ce7e5658ecfb00f2e7
Håkon Antonsen <haakon@antons1.net>
2017-02-14 22:24:49 +0100

    Initialise firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "AIzaSyCL7TGM5z24pEt4uSC9pZsuE6u8ORK-79M",
+    authDomain: "trumpalyse.firebaseapp.com",
+    databaseURL: "https://trumpalyse.firebaseio.com",
+    storageBucket: "trumpalyse.appspot.com",
+    messagingSenderId: "751252707175"
+};
+firebase.initializeApp(config);
